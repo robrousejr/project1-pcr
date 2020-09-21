c7ccec76ea1f1a53eb0dff2ed9d79f9c3aaa35f8
yuvrajsubedi59 <ys96@zips.uakron.edu>
2020-09-21 12:29:54 -0400

    Add main.py file and sequence.fasta

---
+++ main.py
@@ -0,0 +1,27 @@
+#Introduction to bioinformatics
+#Project 1
+#PCR Simulation with Python
+#Genome E
+#26245-26472
+#protein_id="YP_009724392.1"
+
+import os
+import numpy
+
+file = "sequence.fasta"
+
+#print lines retrieving
+
+comments = file.readline()
+
+SARS_COV2_genome = file.read()
+
+file.close()
+
+SARS_COV2_genome = SARS_COV2_genome.replace('\n','')
+
+# extract E gene from 21563:25384
+S_gene = SARS_COV2_genome[26245:26472]   # rna sequence
+
+
+
