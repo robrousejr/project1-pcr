92afb714416281eacefda710a7395a91b9ceb174
Rob <robrousejr@gmail.com>
2020-09-21 16:54:19 -0400

    Add function for getting cdna

--- main.py
+++ main.py
@@ -8,6 +8,17 @@
 import os
 import numpy
 
+# Returns CDNA from RNA
+def getCDNA(rna):
+    cdna = rna.upper() # Ensure all caps to prevent errors
+    cdna = cdna.replace("A", "X")
+    cdna = cdna.replace("T", "A")
+    cdna = cdna.replace("X", "T")
+    cdna = cdna.replace("C", "X")
+    cdna = cdna.replace("G", "C")
+    cdna = cdna.replace("X", "G")
+    return cdna
+
 file = "sequence.fasta"
 
 #print lines retrieving
@@ -22,6 +33,5 @@ SARS_COV2_genome = SARS_COV2_genome.replace('\n','')
 
 # extract E gene from 26245:26472
 E_gene = SARS_COV2_genome[26245:26472]   # rna sequence
-
 
 
