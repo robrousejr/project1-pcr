569037d8467265762d2b5f2008d9ed9971f58999
yuvrajsubedi59 <ys96@zips.uakron.edu>
2020-09-24 23:03:50 -0400

    Add denaturation function

--- main.py
+++ main.py
@@ -4,15 +4,15 @@
 #Genome E
 #26245-26472
 #protein_id="YP_009724392.1"
-"""
-Rob Rouse
-Yuvraj Subedi
-Jaron Smith
-"""
+#"""
+#Rob Rouse
+#Yuvraj Subedi
+#Jaron Smith
+#"""
 
 import os
 import numpy
-from pcr import *
+import pcr as pcr
 
 with open('sequence.fasta') as file:
     comments = file.readline()
@@ -28,4 +28,10 @@ E_gene = SARS_COV2_genome[26245:26472]   # rna sequence
 # Primer pair 7 (Sequence, GC, start, end)
 forwardPrimer = ("CATTCGTTTCGGAAGAGACAGG", 0.5, 7, 28)
 reversePrimer = ("ATTGCAGCAGTACGCACACA", 0.5, 134, 115)
+primers = [forwardPrimer, reversePrimer]
 
+print(E_gene)
+print
+print(len(E_gene))
+print
+print(primers[1])
--- pcr.py
+++ pcr.py
@@ -9,3 +9,21 @@ def getCDNA(rna):
     cdna = cdna.replace("X", "G")
     cdna = cdna[::-1]  # reverse complementary strand
     return cdna
+
+# param: a list of tuples of 2 strs, representing double stranded dna segments
+# return: a list of single strand dna segments
+def denaturation(dna_segments):
+    single_strands_DNA = []
+    for item in dna_segments:
+        single_strands_DNA.append(item[0])
+        single_strands_DNA.append(item[1])
+    return single_strands_DNA
+
+# param: a list of single strand dna segments, each segment is from 5" to 3"
+# return: a list of tuples of 2 strs (2 dna segments from 5" to 3")
+def annealing_elongation(singleStrandDNAs, primers, fall_of_rate = 50):
+    # ...
+    f_primer = primers[0]
+    r_primer = primers[1]
+    doubleStrandedDNAs = [('a','a'),('a','a')]  # get your sequence of dnas
+    return doubleStrandedDNAs
